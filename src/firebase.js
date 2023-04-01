90e1784b502c76cab75f37c1932aee70e5b8887f
Upshur06 <mupshur2005@gmail.com>
2023-03-31 23:26:46 -0400

    starting point

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBgntUBmQeFe3nEVBq0hhPrxPSGxWWNl_I",
+    authDomain: "soulful-scrambles-cafe.firebaseapp.com",
+    projectId: "soulful-scrambles-cafe",
+    storageBucket: "soulful-scrambles-cafe.appspot.com",
+    messagingSenderId: "309275961244",
+    appId: "1:309275961244:web:aa09cfde80ca64a3b3ca59",
+    measurementId: "G-7M20MBNMN9"
+};
