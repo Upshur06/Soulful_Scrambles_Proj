c32ae420041543ba21b62ca2903382a3f0f029be
Upshur06 <mupshur2005@gmail.com>
2023-04-03 13:12:45 -0400

    Breakfast Product menu info

--- src/Breakfast_Product.js
+++ src/Breakfast_Product.js
@@ -14,14 +14,14 @@ function Breakfast_Product() {
                 <div className='breafast_price'>
                     <p>
                         <small>$</small>
-                        <strong>$14.65</strong>
+                        <strong>14.65</strong>
                     </p>
                 </div>
             </div>
-            <div className='breafast_img'>
-
+            <div className='breafast_imgContainer'>
+                <img className='breakfast_img' src='https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YnJlYWtmYXN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60' />
+                <button>Add to cart</button>
             </div>
-        
         </div>
     )
 }
