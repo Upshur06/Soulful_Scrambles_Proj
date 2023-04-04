27b77a4258564d64a94a457346affb52e2924428
Upshur06 <mupshur2005@gmail.com>
2023-04-04 11:57:48 -0400

    checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,10 +5,13 @@ function Checkout() {
     return (
         <div className='checkout'>
             <div className='checkout__left'>
-                <img src='https://media.istockphoto.com/id/1389175014/photo/various-types-of-bread-ears-of-wheat-flying-on-gray-background-classic-wheat-round-bread.jpg?b=1&s=170667a&w=0&k=20&c=XmCULiVsIJFQpexx_EnYGz0DIDYEM3g7giB3rZNwbNQ=' />
+                <img className='checkout_ad' src='https://media.istockphoto.com/id/1389175014/photo/various-types-of-bread-ears-of-wheat-flying-on-gray-background-classic-wheat-round-bread.jpg?b=1&s=170667a&w=0&k=20&c=XmCULiVsIJFQpexx_EnYGz0DIDYEM3g7giB3rZNwbNQ=' />
+                <div>
+                    <h2 className='checkout__title'>Your Order</h2>
+                </div>
             </div>
             <div className='checkout__right'>
-
+                <h2 className=''>Sub Total</h2>
             </div>
         </div>
     )
