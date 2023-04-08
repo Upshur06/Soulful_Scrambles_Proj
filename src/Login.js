244d6ce4d9330f397c140e8f3150ae955ae55284
Upshur06 <mupshur2005@gmail.com>
2023-04-08 02:28:27 -0400

    login and register inputs and btns

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Login.css'
 
 function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='login'>
             <div className='login__Container'>
@@ -9,17 +21,17 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e=> setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e=> setPassword(e.target.value)} />
 
-                    <button className='signIn'>Sign In</button>
+                    <button type="submit" className='signIn' onClick={signIn}>Sign In</button>
                 </form>
 
                 <p>By signing-in you agree to Soulful Scrambles Cafe's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads</p>
 
-                <button className='login__RegisterButton'>Sign-Up</button>
+                <button className='login__RegisterButton' onClick={register}>Sign-Up</button>
 
         </div>
         </div>
